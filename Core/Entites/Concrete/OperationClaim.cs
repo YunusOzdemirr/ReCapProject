063efb62e52e705c38c90cb8fbcc711d3a092cd5
Yunus <yunusozdemir468@gmail.com>
2021-03-16 21:05:53 +0300

    Jwt added

--- Core/Entites/Concrete/OperationClaim.cs
+++ Core/Entites/Concrete/OperationClaim.cs
@@ -6,6 +6,6 @@ namespace Core.Entities.Concrete
     public class OperationClaim:IEntity
     {
         public int Id { get; set; }
-        public int Name { get; set; }
+        public string Name { get; set; }
     }
 }
